58f4a563ce973618b329716db04b9a81b36d0575
Ankit <ankitanand2909@gmail.com>
2021-02-27 23:22:06 +0530

    Add login and registration views

---
+++ food/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class LoginSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = User
+        password = serializers.CharField(required=True)
+        email = serializers.CharField(required=True)
+        fields = ['email','password']
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email','first_name','last_name')
+        write_only_fields = ('password',)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            )
+
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- food/urls.py
+++ food/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from food.views import *
+from django.conf.urls import url
+
 urlpatterns = [
     path('',views.home,name='home'),
     path('signup', views.handleSignup, name='handleSignup'),
     path('login', views.handleLogin, name='handleLogin'),
-    path('logout', views.handleLogout, name='handleLogout')
+    path('logout', views.handleLogout, name='handleLogout'),
+    url(r'^api/login/$',login.as_view(),name='login'),
+    url(r'^api/register/$',RegisterUserView.as_view(),name='register new user'),
 ]
