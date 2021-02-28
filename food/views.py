88af96e1eda99ef5d822e3644cc2250175abf51c
Aniket Pratyush <51285661+lucifer-199@users.noreply.github.com>
2021-02-28 22:54:47 +0530

    Finaall Commmit

--- food/views.py
+++ food/views.py
@@ -12,7 +12,6 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .models import *
 from rest_framework.views import APIView
-
 import datetime
 
 
@@ -44,7 +43,6 @@ def handleSignup(request):
             return redirect('home')
 
         #create the user
-        # myuser = User.objects.create_user(username, email, pass1)
         myuser = User.objects.create_user(username,email,pass1)
         myuser.first_name = fname
         myuser.last_name = lname
@@ -88,53 +86,7 @@ def handleLogout(request):
     messages.success(request,"Successfully logged out!")
     return redirect('home')
 
-
-# class login(APIView):
-#     #permission_classes = (AllowAny,)
-#     def post(self,request):
-#         serializers = LoginSerializer(data=request.data)
-#         if(serializers.is_valid()):
-#             email=serializers.data.get("email")
-#             password = serializers.data.get("password")
-#             self.object = User.objects.filter(email=email).first()
-#             if self.object == None:
-#                 return Response({'error':'User with this email and password not found'})
-#             else:
-#                 if self.object.check_password(password):
-#                     authenticate(self.object)
-#                     self.object.last_login = datetime.datetime.now()
-#                     self.object.save(update_fields=['last_login'])
-#                     val={}
-#                     refresh = RefreshToken.for_user(User.objects.filter(email=serializers.data.get("email")).first())
-#                     val['refresh'] = str(refresh)
-#                     val['access'] = str(refresh.access_token)
-#                     val['Id']= self.object.id
-#                     return Response(val)
-#                 else:
-#                     return Response({'error':'Incorrect password'})
-#         else:
-#             return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class RegisterUserView(APIView):
-#     def post(self, request):
-#         serializers=RegisterUserSerializer(data=request.data)
-#         val= {}
-#         self.object = User.objects.filter(email=request.data.get('email')).first()
-#         if self.object is None:
-#             if serializers.is_valid():
-#                 users=serializers.save()
-#                 val['Response']="Successfully registered a new user"
-#                 # val['Token']=Token.objects.get(user=users).key
-#             else:
-#                 val= serializers.errors
-#             return Response(val)
-#         else:
-#             return Response({'error':"Email already in use."})
-
-
 class DataView(APIView):
-    # authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]     
     def get(self, request):
         queryset= Food.objects.all()
