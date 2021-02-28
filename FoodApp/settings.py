2062a705d6222fc123fe9f035d0003be58a7d912
Ankit <ankitanand2909@gmail.com>
2021-02-28 21:01:30 +0530

    login update

--- FoodApp/settings.py
+++ FoodApp/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'food.apps.FoodConfig'
+    'food.apps.FoodConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
